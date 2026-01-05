65503f981ea05eb6e7a39e73fdd7230c6b357722
KishanPatel58 <patelkishan3101@gmail.com>
2026-01-05 09:34:38 +0530

    ch10

---
+++ demo.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Hello Streamlit",
+    page_icon="😁",
+    layout="centered"
+)
+st.title("welcome to streamlit")
+st.header("this is a header")
+st.subheader("this is sub header")
+st.text("this is text")
+st.write("this is write")
+st.markdown("**this is markdown")
+# display python
